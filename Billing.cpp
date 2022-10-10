d64cc04779218d90ef488871f10a30fefe360b41
dvimukthi <deshanvimukthi59@gmail.com>
2022-10-10 21:31:51 +0530

    rem function

--- Billing.cpp
+++ Billing.cpp
@@ -254,11 +254,58 @@ void shopping::edit()
         data1.close();
 
         remove("database.txt");
-        remove("database1.txt", "database.txt");
+        rename("database1.txt", "database.txt");
 
         if (token == 0)
         {
-            cout << "\n\n Record Not Found Sorry! ";
+            cout << "\n\n Record Not Found! ";
+        }
+    }
+}
+
+void shopping::rem()
+{
+    fstream data, data1;
+    int pkey;
+    int token = 0;
+
+    cout << "\n\n\t Delete Product";
+    cout << "\n\n\t Product Code";
+    cin >> pkey;
+
+    data.open("database.txt", ios::in);
+
+    if (!data)
+    {
+        cout << "File Doesn't Exist!";
+    }
+    else
+    {
+        data1.open("database1.txt", ios::app | ios::out);
+        data >> pcode >> pname >> price >> dis;
+
+        while (!data.eof())
+        {
+            if (pcode == pkey)
+            {
+                cout << "\n\n\t Product Deleted Successfully";
+                token++;
+            }
+            else
+            {
+                data1 << " " << pcode << " " << pname << " " << price << " " << dis << "\n";
+            }
+            data >> pcode >> pname >> price >> dis;
+        }
+        data.close();
+        data1.close();
+
+        remove("database.txt");
+        rename("database1.txt", "database.txt");
+
+        if (token == 0)
+        {
+            cout << "\n\n Record Not Found! ";
         }
     }
 }
