ec5faa6f185e56cbe9d9aafbe8f39fd9b4145a7a
dvimukthi <deshanvimukthi59@gmail.com>
2022-10-11 11:55:38 +0530

    receipt function

--- Billing.cpp
+++ Billing.cpp
@@ -326,3 +326,79 @@ void shopping::list()
     }
     data.close();
 }
+
+void shopping::receipt()
+{
+    fstream data;
+    int arrc[100];
+    int arrq[100];
+    char choice;
+    int c = 0;
+    float amount = 0;
+    float dis = 0;
+    float total = 0;
+
+    cout << "\n\n\t\t\t\t RECEIPT ";
+    data.open("database.txt", ios::in);
+
+    if (!data)
+    {
+        cout << "\n\n Empty Database";
+    }
+    else
+    {
+        data.close();
+
+        list();
+        cout << "\n_____________________________________\n";
+        cout << "\n |                                   \n";
+        cout << "\n       Please PLace The Order        \n";
+        cout << "\n |                                   \n";
+        cout << "\n_____________________________________\n";
+
+        do
+        {
+            cout << "\n\nEnter Product Code : ";
+            cin >> arrc[c];
+            cout << "\n\nEnter The Product Quantity : ";
+            cin >> arrq[c];
+
+            for (int i = 0; i < c; i++)
+            {
+                if (arrc[c] == arrc[i])
+                {
+                    cout << "\n\n Duplicate Product Code... Please Try Again!";
+                    goto m;
+                }
+            }
+            c++;
+            cout << "\n\n Do You Want To Buy Another Product? If Yes Then Press Y Else No";
+            cin >> choice;
+        } while (choice == "y");
+
+        cout << "\n\n\t\t\t____________________RECEIPT____________________\n";
+        cout << "\nProduct No\t Product Name\t Product Quantity\tPrice\tAamount\tAmount With Discount\n";
+
+        for (int i = 0; i < c; i++)
+        {
+            data.open("database.txt", ios::in);
+            data >> pcode >> pname >> price >> dis;
+
+            while (!data.eof())
+            {
+                if (pcode == arrc[i])
+                {
+                    amount = price * arrq[i];
+                    dis = amount - (amount * dis / 100);
+                    total = total + dis;
+                    cout << "\n"
+                         << pcode << "\t\t" << pname << "\t\t" << arrq[i] << "\t\t" << price << "\t" << amount << "\t\t" << dis;
+                }
+                data >> pcode >> pname >> price >> dis;
+            }
+        }
+        data.close();
+    }
+    cout << "\n\n_______________________________________________________";
+    cout << "\n Total Amount : " << total;
+}
