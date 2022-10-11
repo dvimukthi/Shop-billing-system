b508aad0aef80c991987ccd7e319086b356c89df
dvimukthi <deshanvimukthi59@gmail.com>
2022-10-11 11:18:47 +0530

    list function

--- Billing.cpp
+++ Billing.cpp
@@ -309,3 +309,20 @@ void shopping::rem()
         }
     }
 }
+
+void shopping::list()
+{
+    fstream data;
+    data.open("database.txt", ios::in);
+    cout << "\n\n|______________________________________________";
+    cout << "ProNo\t\tName\t\tPrice"\n;
+    cout << "\n\n|______________________________________________";
+    data >> pcode >.pname >> price >> dis;
+
+    while (!data.eof())
+    {
+        cout << pcode < "\t\t" << pname << "\t\t" << price << "\n";
+        data >> pcode >> pname >> price >> dis;
+    }
+    data.close();
+}
